8cc5511b74b529aed11ccde57755eaea7c686772
xinyubian <2017118102@stu.hstc.edu.cn>
2019-10-23 09:33:10 +0800

    调用地图界面

--- HelloWorld/app/src/main/java/com/example/helloworld/HelloWorldActivity.java
+++ HelloWorld/app/src/main/java/com/example/helloworld/HelloWorldActivity.java
@@ -36,8 +36,8 @@ public class HelloWorldActivity extends AppCompatActivity implements View.OnClic
             public void onClick(View v) {
 //                Intent intent = new Intent(HelloWorldActivity.this, Activity2.class);
 //                startActivity(intent);
-                  Intent intent=new Intent(Intent.ACTION_DIAL);
-                  intent.setData(Uri.parse("tel:10086"));
+                  Intent intent=new Intent(Intent.ACTION_VIEW);
+                  intent.setData(Uri.parse("geo:"));
                   startActivity(intent);
             }
         });
